c0187e0be8d8940fb9dfcb2443b232434117a4df
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2025-09-06 16:38:44 +0700

    update: sua loi nav khong chi dung tab

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -25,46 +25,47 @@ const categories = [
 ];
 
 export function Navbar({ mobile = false }: { mobile?: boolean }) {
-    const [isHovered, setIsHovered] = useState(false);
-    const pathname = usePathname();
 
-    if (mobile) {
-        return (
-            <nav className="flex flex-col gap-4 mt-8">
-                {navigation.map((item: NavigationType) => (
-                    <Link
-                        href={item.href}
-                        key={item.id}
-                        className={`text-lg font-bold text-gray-700 hover:text-lime-600 px-4 py-3 rounded transition-all ${pathname === item.href ? 'bg-lime-100' : ''}`}
-                    >
-                        {item.name}
-                    </Link>
-                ))}
-            </nav>
-        );
-    }
+  const pathname = usePathname();
+  const segments = pathname.split("/").filter(Boolean);
 
+  if (mobile) {
     return (
-        <div className="navbar container mx-auto relative">
-            <div className="flex items-center space-x-6">
-                {/* Navigation links */}
-                <nav className="flex items-center gap-4">
-                    {navigation.map((item: NavigationType) => (
-                        <Link
-                            href={item.href}
-                            key={item.id}
-                            className={`
-                                relative text-sm px-1 py-2 transition-colors duration-200
-                                ${pathname === item.href || (pathname === '/' && item.href === '/')
-                                    ? "text-black after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-yellow-500"
-                                    : "text-gray-800 hover:text-black after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-black after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-out after:origin-left"}
-                                `}
-                        >
-                            {item.name}
-                        </Link>
-                    ))}
-                </nav>
-            </div>
-        </div>
+      <nav className="flex flex-col gap-4 mt-8">
+              {navigation.map((item: NavigationType) => (
+                  <Link
+                      href={item.href}
+                      key={item.id}
+                      className={`text-lg font-bold text-gray-700 hover:text-lime-600 px-4 py-3 rounded transition-all ${pathname === item.href ? 'bg-lime-100' : ''}`}
+                  >
+                      {item.name}
+                  </Link>
+              ))}
+          </nav>
     );
+  }
+
+  return (
+    <div className="navbar container mx-auto relative">
+      <div className="flex items-center space-x-6">
+        {/* Navigation links */}
+        <nav className="flex items-center gap-4">
+          {navigation.map((item: NavigationType) => (
+            <Link
+              href={item.href}
+              key={item.id}
+              className={`
+                relative text-sm px-1 py-2 transition-colors duration-200
+                ${("/" + segments[0]) === item.href || (pathname === '/' && item.href === '/')
+                    ? "text-black after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-yellow-500"
+                    : "text-gray-800 hover:text-black after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-black after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:ease-out after:origin-left"}
+                `}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
 }
